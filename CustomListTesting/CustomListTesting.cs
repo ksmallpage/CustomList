8073580664baa21d5f254e8b4419d6b9edd30f9c
keith smallpage <keith.smallpage@icloud.com>
2018-03-01 14:12:08 -0600

    setup of Project Intial commit

---
+++ CustomListTesting/CustomListTesting.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using CustomLists;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CustomListTesting
+{
+    [TestClass]
+    public class CustomListTesting
+    {
+        [TestMethod]
+        public void UnitTests()
+        {
+        }
+    }
+}
---
+++ CustomLists/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomLists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomListClass customlistclass = new CustomListClass();
+
+            List<int> testlist = new List<int>();
+
+            testlist.Add(12);
+            Console.WriteLine(testlist[0]);
+            Array myArray = new Array[10];
+            
+        }
+    }
+}

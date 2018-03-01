cf46801c1900ccec8450516db214a89e74834c78
keith smallpage <keith.smallpage@icloud.com>
2018-03-01 16:02:15 -0600

    created Add Test Method

--- CustomListTesting/CustomListTesting.cs
+++ CustomListTesting/CustomListTesting.cs
@@ -8,8 +8,77 @@ namespace CustomListTesting
     public class CustomListTesting
     {
         [TestMethod]
-        public void UnitTests()
+
+        static void CustomeList_AddAnIntObjectToCustomList_ReturnsIntObjectinIndex0()
         {
+            CustomList<int> custlist = new CustomList<int>();
+            // arrange
+
+            int expected = 30;
+            int actual;
+
+            //act
+            custlist.Add(30);
+            actual = custlist[0];
+            //assert
+
+            Assert.AreEqual(expected, actual);
+
         }
+
+        [TestMethod]
+
+        static void CustomeList_AddAnIntObjectToCustomList_ReturnsIntObjectinIndex1()
+        {
+            CustomList<int> custlist = new CustomList<int>();
+            // arrange
+
+            int expected = 20;
+            int actual;
+
+            //act
+            custlist.Add(20);
+            actual = custlist[2];
+            //assert
+
+            Assert.AreEqual(expected, actual);
+
+        }
+
+        [TestMethod]
+
+        static void CustomeList_AddAnReftypeToCustomList_ReturnsRefTypeinIndex0()
+        {
+            CustomList<> custlistref = new CustomList<int>();
+            // arrange
+
+            int expected = 30;
+            int actual;
+
+            //act
+            actual = custlist.Add(ref 30);
+            //assert
+
+            Assert.AreEqual(expected, actual);
+
+        }
+
+
+        //[TestMethod]
+        //[ExpectedException(typeof(ArgumentOutOfRangeException))]
+        //public void Weather_OutsideRangeIndex_Exception()
+        //{
+        //    Weather weather = new LemonAidStand.Weather();
+        //    // arrange
+
+        //    //  string expected = "Muggy";
+        //    string actual;
+
+        //    //act
+        //    actual = weather.CreateWeather(6);
+        //    //assert
+
+        //    //  Assert.AreEqual(expected, actual);
+
     }
-}
+    }
--- CustomLists/CustomList.cs
+++ CustomLists/CustomList.cs
@@ -6,7 +6,75 @@ using System.Threading.Tasks;
 
 namespace CustomLists
 {
-    class CustomList
+ public class CustomList<T>
     {
+
+        // member variables (HAS A)
+        T[] items = new T[10] ;
+        
+
+        // constructor (SPAWNER)
+        //  public CustomListClass<T>
+        //{
+
+        //}
+
+        // member methods (CAN DO)
+
+        // Need Add Method
+        public void Add (T item)
+        {
+           
+        }
+
+        // Need an Remove object Method
+
+        public void Remove(T item)
+        {
+
+        }
+
+        // Need a loop thro list Method
+
+        public void Loop()
+        {
+
+        }
+
+        // Override a Tostring Method
+
+        public void CToString()
+        {
+
+        }
+
+        // Overload a + opeartor tp add two instance of custom list
+
+        public void Plus()
+        {
+
+        }
+
+        // Count Method
+        public void Count()
+        {
+
+        }
+
+        // Zip Method
+        public void InterLeave()
+        {
+
+        }
+
+        // Sort Metod for Bonus
+
+        public void Sort()
+        {
+
+        }
+
+
+
     }
 }
